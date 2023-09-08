a4dcd58d89cfdf32cf94a635d5ecb22556f0defe
VicenteFarias10 <vi.fariasr@duocuc.cl>
2023-09-08 14:09:18 -0300

    segundo commit

--- src/app/animations/animations.component.ts
+++
@@ -1,8 +0,0 @@
-import { trigger, transition, animate, style } from '@angular/animations';
-
-export const slideInAnimation = trigger('routeAnimations', [
-  transition('* <=> *', [
-    style({ position: 'relative' }),
-    animate('300ms ease-in-out', style({ left: '0' })),
-  ]),
-]);
